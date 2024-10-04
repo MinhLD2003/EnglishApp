95c6f9e142b266697e9510b9ed0def801876dc0d
Mon1712 <133070020+Mon1712@users.noreply.github.com>
2024-10-04 10:31:27 +0700

    push-code-fix

--- app/src/main/java/com/man/learningenglish/MainActivity.java
+++ app/src/main/java/com/man/learningenglish/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login);
 
         Register = (Button) findViewById(R.id.DangKy);
         Login = (Button) findViewById(R.id.DangNhap);
